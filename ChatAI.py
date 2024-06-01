2dc69f20e9e0b9f9e49ff06742226795c84ec027
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-06-01 22:01:06 +0530

    streamlit

--- ChatAI.py
+++ ChatAI.py
@@ -1,7 +1,12 @@
-openai.api_key = "OPENAI_API_KEY"  # Put your own generated openai-api-key
+# import openai
 
+# openai.api_key = "OPENAI_API_KEY"  # Put your own generated openai-api-key
+
+
+# def chat(query):
+#     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": query}])
+
+#     return response.choices[0].message.content
 
 def chat(query):
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": query}])
-
-    return response.choices[0].message.content
+    return "Work in progresss!!"
